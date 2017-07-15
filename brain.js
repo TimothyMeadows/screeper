21bf6f6d63007567768fc7803acb9ba8cc5c8aa2
Timothy D Meadows II <timothy@timothymeadows.com>
2017-07-14 21:34:20 -0700

    update core brain structure of bot for creation / destruction of objects

--- brain.js
+++ brain.js
@@ -1,19 +1,20 @@
 var BrainTemplate = {
     name: "example",
+    // Triggers on creation / destruction based on disposed
     spawn: function (spawn, disposed) {
         // ...
     },
-    // Triggers on each game "cycle" defined by speed of server / overload for each creep
+    // Triggers on creation / destruction based on disposed
     creep: function (creep, disposed) {
         // ...
     },
-    // Triggers on each game "cycle" defined by speed of server / overload for each tower structure
-    tower: function(tower, disposed) {
+    // Triggers on creation / destruction based on disposed
+    structure: function(structure, disposed) {
         // ...
     },
     // Triggers on cpu limit being reached, other triggers will be skipped until the next tick to allow time to catch up
     overload: function() {
-
+        // ...
     }
 };
 
--- main.js
+++ main.js
@@ -4,45 +4,30 @@ require("extention.source");
 require("extention.creep");
 
 var brain = require("brain");
-Memory.structures = {};
-
 module.exports.loop = function () {
     if (Game.cpu.bucket < Game.cpu.tickLimit * 2) {
         if (brain.overload) brain.overload();
         return;
     }
 
-    var name, diff;
-    diff = global.sync(Memory.spawns, Game.spawns);
-    if (diff.left.length > 0) {
-        for (name in diff.left)
-            if (brain.spawn) brain.spawn(Memory.spawns[name], true);
-    }
+    for (name in Game.rooms) {
+        var room = Game.rooms[name], name, diff;
+        if (!room.memory.room) room.memory.room = room.name;
+        if (!room.memory.structures) room.memory.structures = {};
+        if (!room.memory.creeps) room.memory.creeps = {};
 
-    if (diff.right.length > 0) {
-        for (name in diff.left)
-            if (brain.spawn) brain.spawn(Game.spawns[name], false);
-    }
+        // Synchronize memory with game state
+        for (name in Game.structures) {
+            var structure = Game.structures[name];
+            if (!structure.room || structure.room.name != room.name)
+                continue;
 
-    diff = global.sync(Memory.structures, Game.structures);
-    if (diff.left.length > 0) {
-        for (name in diff.left)
-            if (brain.structure) brain.structure(Memory.structures[name], true);
-    }
+            if (!room.memory.structures[structure.id]) {
+                room.memory.structures[structure.id] = structure;
+                if (brain.structure) brain.structure(structure, false);
+            }
+        }
 
-    if (diff.right.length > 0) {
-        for (name in diff.left)
-            if (brain.structure) brain.structure(Game.structures[name], false);
-    }
-
-    diff = global.sync(Memory.creeps, Game.creeps);
-    if (diff.left.length > 0) {
-        for (name in diff.left)
-            if (brain.creep) brain.creep(Memory.creeps[name], true);
-    }
-
-    if (diff.right.length > 0) {
-        for (name in diff.left)
-            if (brain.creep) brain.creep(Game.creeps[name], true);
+        if (brain.tick) brain.tick(room);
     }
 }
