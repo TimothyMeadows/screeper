64548012ed21604e917ed2337acf352d305d4529
Timothy D Meadows II <timothy@timothymeadows.com>
2017-07-25 21:38:20 -0700

    main class

---
+++ screeper.js
@@ -0,0 +1,47 @@
+var RoomController = require("controller.room");
+var CreepController = require("controller.creep");
+
+var Screeper;
+module.exports = Screeper = {
+    // Triggers on creation / destruction based on disposed
+    creep: function (room, creep, disposed) {
+        if (disposed)
+            room.log(`creep ${creep.id}:${creep.name} has died!`);
+        else
+            room.log(`creep ${creep.id}:${creep.name} was born!`);
+    },
+    // Triggers on creation / destruction based on disposed
+    structure: function(room, structure, disposed) {
+        if (disposed)
+            room.log(`structure ${structure.id}:${structure.type} was destroyed!`);
+        else
+            room.log(`structure ${structure.id}:${structure.type} was created!`);
+    },
+    // Triggers on creation / destruction based on disposed
+    room: function(name, disposed) 
+    {
+        if (disposed) {
+            global.log(`room ${name} was lost!`);
+            RoomController.loss(name);
+        } else {
+            global.log(`room ${name} was gained!`);
+            RoomController.gain(Game.rooms[name]);
+        }
+    },
+    // Triggers on cpu limit being reached, other triggers will be skipped until the next tick to allow time to catch up
+    overload: function() {
+        global.log("cpu is overloaded waiting until next tick.");
+    },
+    // Triggers each game ticks for each room if there cpu.
+    tick: function(room, disposed) {
+        if (!room.controller) {
+            return;
+        }
+
+        RoomController.tick(room);
+
+        for (var name in room.memory.creeps)
+            CreepController.tick(room, room.memory.creeps[name]);
+        
+    }
+};
