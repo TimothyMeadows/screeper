0dbb1431067c0a842b289f8dbb6339e5d8c92c44
Timothy D Meadows II <timothy@timothymeadows.com>
2017-09-04 10:02:04 -0700

    Split controller.creep for multi room support.

--- controller.creep.js
+++ controller.creep.js
@@ -1,139 +1,14 @@
+var CreepControllerBase = require("controller.creep.base");
+
 var CreepController;
 module.exports = CreepController = {
     tick: function (room, pointer) {
-        if (pointer.task.idle === true) {
-            var creep = Game.getObjectById(pointer.id);
-            if (!creep)
-                return;
+        if (room.memory.map.type === "base")
+            CreepControllerBase.tick(room, pointer);
 
-            switch (pointer.caste.name) {
-                case "worker":
-                    switch (pointer.caste.specialization) {
-                        case "miner":
-                            if (creep.carry.energy < creep.carryCapacity) {
-                                creep.change("energy-miner", true);
-                            } else {
-                                if (room.population("religious") === 0) {
-                                    creep.change("energy-distributor", true);
-                                }
-                            }
-                            break;
-                        case "builder":
-                            if (room.priorityRepairs() === 0) {
-                                if (room.prioritySites() === 0) {
-                                    if (room.sites() === 0) {
-                                        if (creep.carry.energy < creep.carryCapacity) {
-                                            if (room.population("religious") === 0) {
-                                                creep.change("energy-miner", true);
-                                            } else {
-                                                creep.change("energy-collector", true);
-                                            }
-                                        } else {
-                                            if (room.population("religious") === 0) {
-                                                creep.change("energy-distributor", true);
-                                            } else {
-                                                if (room.repairs() === 0) {
-                                                    creep.change("upgrade-controller", true);
-                                                } else {
-                                                    creep.change("repairer", true);
-                                                }
-                                            }
-                                        }
-                                    } else {
-                                        if (creep.carry.energy < creep.carryCapacity) {
-                                            if (room.population("religious") === 0) {
-                                                creep.change("energy-miner", true);
-                                            } else {
-                                                creep.change("energy-collector", true);
-                                            }
-                                        } else {
-                                            creep.change("builder", true);
-                                        }
-                                    }
-                                } else {
-                                    if (creep.carry.energy < creep.carryCapacity) {
-                                        if (room.population("religious") === 0) {
-                                            creep.change("energy-miner", true);
-                                        } else {
-                                            creep.change("energy-collector", true);
-                                        }
-                                    } else {
-                                        creep.change("priority-builder", true);
-                                    }
-                                }
-                            } else {
-                                if (creep.carry.energy === 0) {
-                                    if (room.population("religious") === 0) {
-                                        creep.change("energy-miner", true);
-                                    } else {
-                                        creep.change("energy-collector", true);
-                                    }
-                                } else {
-                                    creep.change("priority-repairer", true);
-                                }
-                            }
-                            break;
-                        case "contractor":
-                            if (creep.carry.energy < creep.carryCapacity) {
-                                if (room.population("religious") === 0) {
-                                    creep.change("energy-miner", true);
-                                } else {
-                                    creep.change("energy-collector", true);
-                                }
-                            } else {
-                                if (room.population("religious") === 0) {
-                                    creep.change("energy-distributor", true);
-                                } else {
-                                    creep.change("upgrade-controller", true);
-                                }
-                            }
-                            break;
-                    }
-                    break;
-                case "religious":
-                    switch (pointer.caste.specialization) {
-                        case "prior":
-                            if (creep.carry.energy < creep.carryCapacity) {
-                                creep.change("energy-collector", true);
-                            } else {
-                                creep.change("energy-distributor", true);
-                            }
-                            break;
-                        case "healer":
-                            if (creep.carry.energy < creep.carryCapacity) {
-                                creep.change("energy-collector", true);
-                            } else {
-                                creep.change("energy-distributor", true);
-                            }
-                            break;
-                        case "ranger":
-                            if (creep.carry.energy < creep.carryCapacity) {
-                                creep.change("energy-collector", true);
-                            } else {
-                                creep.change("energy-distributor", true);
-                            }
-                            break;
-                    }
-                    break;
-                case "warrior":
-                    switch (pointer.caste.specialization) {
-                        case "purifier":
-                            if (room.hostiles() === 0) {
-                                creep.change("protect-controller", true);
-                            } else {
-                                creep.change("ranged-kiting", true);
-                            }
-                            break;
-                        case "guardian":
-                            break;
-                        case "vanguard":
-                            break;
-                    }
-                    break;
-            }
+        if (pointer.task) {
+            var task = require(`task.${pointer.task.name}`);
+            task.tick(room, pointer);
         }
-
-        var task = require(`task.${pointer.task.name}`);
-        task.tick(room, pointer);
     }
 };
