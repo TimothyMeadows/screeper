c6a06e59c297e8b40853e87f583de3b4b8070c2a
Timothy D Meadows II <timothy@timothymeadows.com>
2019-06-27 17:25:24 -0700

    Add extension.room.

---
+++ extension.room.js
@@ -0,0 +1,26 @@
+Room.prototype.findExit = function(target) {
+    return Game.map.findExit(this.name, target);
+};
+
+Room.prototype.findExits = function() {
+    return Game.map.describeExits(this.name);
+};
+
+Room.prototype.build = function (type, x, y) {
+    var position = new RoomPosition(x, y, this.name);
+    var tile = Game.map.getTerrainAt(position);
+
+    switch (tile) {
+        case "swamp":
+        case "plain":
+            position.createConstructionSite(type);
+            break;
+        default:
+            this.log(`Can't create construction site, tile: ${tile}, x: ${x}, y: ${y}`);
+            break;
+    };
+};
+
+Room.prototype.log = function (value) {
+    Debug.log(`${value}, room: ${this.name}`);
+};
